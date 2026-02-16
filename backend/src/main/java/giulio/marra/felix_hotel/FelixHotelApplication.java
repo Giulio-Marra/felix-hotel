08e52e30a64359979765249c4ce38076c56ac31d
Giulio-Marra <giuliomarramail@gmail.com>
2026-02-16 14:26:36 +0100

    Setup iniziale backend con Spring Boot

---
+++ backend/src/main/java/giulio/marra/felix_hotel/FelixHotelApplication.java
@@ -0,0 +1,13 @@
+package giulio.marra.felix_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelixHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelixHotelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/giulio/marra/felix_hotel/FelixHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package giulio.marra.felix_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelixHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
